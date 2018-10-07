8c944afb28b2051a8aedda360c219ca70acaa7c2
BazT <bazetug@gmail.com>
2018-10-06 18:50:58 -0700

    models updated

--- server/models/rim.js
+++ server/models/rim.js
@@ -2,14 +2,13 @@ var mongoose = require('mongoose');
 // 
 
 var rimSchema = new mongoose.Schema({
-    title:  { type: String, required: [true, 'Title is required']},
-    brand:  { type: String, required: [true, 'Brand is required']},
-    model:  { type: String, required: [true, 'Model is required']},
-    price:  { type: Number },
-    year:  { type: Number},
-    condition:  { type: String},
-    description: {type: String},
-    photos: { type: Array, "default" : []}
+    diameter: { type: Number},
+    brand: { type: String },
+    condition: { type: String },
+    description: { type: String },
+    photos: { type: Array, "default": [] },
+    price: { type: Number},
+    count: { type: String }
 }, {timestamps: true });
 
 var Rim = mongoose.model('Rim', rimSchema) 
--- server/models/tire.js
+++ server/models/tire.js
@@ -2,14 +2,15 @@ var mongoose = require('mongoose');
 // 
 
 var tireSchema = new mongoose.Schema({
-    brand:  { type: String, required: [true, 'Brand is required']},
-    model:  { type: String, required: [true, 'Model is required']},
-    price:  { type: Number },
-    year:  { type: Number},
-    color:  { type: String, required: [true, 'Color is required']},
-    mileage:  { type: Number},
-    description: {type: String},
-    photos: { type: Array, "default" : []}
+    width: {type: Number},
+    ratio: {type: Number},
+    diameter: { type: Number },
+    brand: { type: String },
+    condition: { type: String },
+    description: { type: String },
+    photos: { type: Array, "default": [] },
+    price: { type: Number },
+    count: { type: String }
 }, {timestamps: true });
 
 var Tire = mongoose.model('Tire', tireSchema) 
