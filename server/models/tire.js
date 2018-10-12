e3fb3b4a27813551a20fbf5b87570b30a2474d6d
Sergey Sherstobitov <in43sh@gmail.com>
2018-10-11 20:52:38 -0700

    some changes in tire.js

--- server/models/tire.js
+++ server/models/tire.js
@@ -1,16 +1,36 @@
 var mongoose = require('mongoose');
-// 
 
 var tireSchema = new mongoose.Schema({
-    width: {type: Number},
-    ratio: {type: Number},
-    diameter: { type: Number },
-    brand: { type: String },
-    condition: { type: String },
-    description: { type: String },
-    photos: { type: Array, "default": [] },
-    price: { type: Number },
-    count: { type: String }
-}, {timestamps: true });
+  width: {
+    type: Number
+  },
+  ratio: {
+    type: Number
+  },
+  diameter: {
+    type: Number
+  },
+  brand: {
+    type: String
+  },
+  condition: {
+    type: String
+  },
+  description: {
+    type: String
+  },
+  photos: {
+    type: Array,
+    "default": []
+  },
+  price: {
+    type: Number
+  },
+  count: {
+    type: String
+  }
+}, {
+  timestamps: true
+});
 
-var Tire = mongoose.model('Tire', tireSchema) 
+var Tire = mongoose.model('Tire', tireSchema)
