308477f5365b5dbc8fd847baabbab5ac3c178afb
BazT <bazetug@gmail.com>
2018-11-28 21:00:04 -0800

    ladning page text fix

--- src/components/Top/Top.js
+++ src/components/Top/Top.js
@@ -10,9 +10,10 @@ export default class Top extends Component {
             <div className="top-info">
               <h2 className="text-center">ACR TIRE SHOP</h2>
               <p className="text-center">
-                TIRES TIRES TIRES TIRES TIRES TIRES TIRES TIRES
-                TIRES TIRES TIRES TIRES TIRES TIRES TIRES TIRES
-                TIRES TIRES TIRES TIRES TIRES TIRES TIRES TIRES
+              One of the original tire shops in North Seattle. We are your one-stop shop for all of your car care needs.
+              ACR is professionally managed and certified, and we are local independent business.
+              Our facility specializes in new and used wheels and rims. 
+              We dedicate ourselves to provide you with quality work and superior customer service, by our certified and licensed technicians, while using only the highest and finest quality products.
               </p>
             </div>
           </div>
